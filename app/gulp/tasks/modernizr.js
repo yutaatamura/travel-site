e6b7754b65c4397ba304332d9181dfec70bfa9f2
yutaatamura <yutaatamura@gmail.com>
2019-08-15 07:42:38 -0500

    Completed browser support fallbacks

---
+++ app/gulp/tasks/modernizr.js
@@ -0,0 +1,13 @@
+var gulp = require("gulp"),
+  modernizr = require("gulp-modernizr");
+
+gulp.task("modernizr", function() {
+  return gulp
+    .src(["./assets/styles/**/*.css", "./assets/scripts/**/*.js"])
+    .pipe(
+      modernizr({
+        options: ["setClasses"]
+      })
+    )
+    .pipe(gulp.dest("./temp/scripts/"));
+});
